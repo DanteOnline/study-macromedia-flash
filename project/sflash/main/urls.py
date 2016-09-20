7fe0499b821c9c62bddc67940180ee071d37ca93
DanteOnline <iamdanteonline@gmail.com>
2016-09-20 21:04:13 +0300

    add bbcode host fix

--- project/sflash/main/urls.py
+++ project/sflash/main/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from main.views import main_view
+from main.views import main_view, change_image_host
 
 urlpatterns = [
-    url(r'^$',main_view,name='index')
+    url(r'^$',main_view,name='index'),
+    url(r'^fix$', change_image_host, name='fix')
 ]
--- project/sflash/main/views.py
+++ project/sflash/main/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from theory.models import Theory
 
 # Create your views here.
 def main_view(request):
     return redirect('theory',pk=1)
+
+def change_image_host(request):
+    old_host = 'http://127.0.0.1:8000/'
+    #new_host = 'http://gamesflashtest.pythonanywhere.com/'
+    new_host = 'http://127.0.0.1:8000/'
+
+    all_theory = Theory.objects.all()
+    for t in all_theory:
+        t.text = t.text.replace(old_host,new_host)
+        t.save()
+    return redirect('index')
